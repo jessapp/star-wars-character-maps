39bfbd608b71a48b3e583869439c67987748fe57
Jessica Appelbaum <jess.appelbaum@gmail.com>
2017-06-12 21:27:36 +0000

    Initial commit

---
+++ server.py
@@ -0,0 +1,37 @@
+"""Star Wars Character Maps"""
+
+from jinja2 import StrictUndefined
+
+from flask import (Flask, jsonify, render_template, redirect, request, flash,
+                   session)
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+app.secret_key = "oisjdfonero87aw9ed8afsond"
+
+app.jinja_env.undefined = StrictUndefined
+
+@app.route('/')
+def index():
+    """Homepage"""
+
+    return render_template("homepage.html")
+
+
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+    app.jinja_env.auto_reload = app.debug  # make sure templates, etc. are not cached in debug mode
+
+    # connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+
+    
+    app.run(port=5000, host='0.0.0.0')
